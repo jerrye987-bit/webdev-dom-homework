553eebfff753fcfb74b514fad7639fd2a154e3f5
jerrye987-bit <jerrye987@gmail.com>
2026-05-18 20:30:04 +0500

    Сщзданы файлы модулей. JS разбит на модули

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+    
+
+    import { renderComments } from "./modules/renderComments.js";
+    import { initNewComment } from "./modules/initNewComment.js";
+
+    renderComments();
+    initNewComment();
---
+++ modules/renderComments.js
@@ -0,0 +1,64 @@
+import { comments } from "./commentsArr.js";
+
+export const name = document.getElementById("name-input");
+export const text = document.getElementById("text-input");
+
+export const renderComments = () => {
+      const list = document.querySelector('.comments');
+
+      list.innerHTML = comments.map((comment, index) => {
+        return `
+        <li class="comment" data-index="${index}">
+          <div class="comment-header">
+            <div>${comment.name}</div>
+            <div>${comment.date.toLocaleString()}</div>
+          </div>
+          <div class="comment-body">
+            <div class="comment-text">${comment.text}</div>
+          </div>
+          <div class="comment-footer">
+            <div class="likes">
+              <span class="likes-counter">${comment.likes}</span>
+              <button data-index="${index}" class="like-button ${comment.isLiked ? "-active-like" : ""}"></button>
+            </div>
+          </div>
+        </li>
+        `;
+      }).join("");
+      
+      const likeButtons = document.querySelectorAll(".like-button");
+
+      for(const likeButton of likeButtons) {
+        likeButton.addEventListener('click', (event) => {
+          event.stopPropagation();
+
+          const index = likeButton.dataset.index;
+          const comment = comments[index];
+
+          if (comment.isLiked) {
+            comment.likes -= 1;
+          } else {
+            comment.likes += 1;
+          }
+
+          comment.isLiked = !comment.isLiked;
+
+          const likesContainer = likeButton.closest('.likes');
+          const counterElement = likesContainer.querySelector('.likes-counter');
+
+          counterElement.textContent = comment.likes;
+          likeButton.classList.toggle('-active-like', comment.isLiked);
+        })
+      }
+
+      const commentsElements = document.querySelectorAll(".comment");
+
+      for (const commentElement of commentsElements) {
+        commentElement.addEventListener('click', () => {
+          const currentComment = comments[commentElement.dataset.index];
+
+          text.value = `> ${currentComment.text} >\n ${currentComment.name},`;
+          text.focus();
+        })
+      }
+    }
