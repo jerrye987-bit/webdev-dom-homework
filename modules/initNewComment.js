f29eed9d4d4d584e8c4899aa0813b5b984002afe
jerrye987-bit <jerrye987@gmail.com>
2026-05-31 16:52:00 +0500

    Добавлен визуал добавления комментария

--- modules/initNewComment.js
+++ modules/initNewComment.js
@@ -13,12 +13,16 @@ export const initNewComment = () => {
         if (name.value.trim() === '' || text.value.trim() === '') {
             return alert('Пожалуйста, заполните все поля формы!')
         } else {
-            document.querySelector('.form-loading')
+            document.querySelector('.form-loading').style.display = 'block'
+            document.querySelector('.add-form').style.display = 'none'
 
             postComment(
                 sanitizeHtml(text.value),
                 sanitizeHtml(name.value),
             ).then((data) => {
+                document.querySelector('.form-loading').style.display = 'none'
+                document.querySelector('.add-form').style.display = 'flex'
+
                 updateComments(data)
                 renderComments()
                 name.value = ''
