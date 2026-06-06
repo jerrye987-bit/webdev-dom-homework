d85a15428c7facd1459c464f835328d5e926cf45
jerrye987-bit <jerrye987@gmail.com>
2026-06-06 16:47:31 +0500

    Убрана вложенность get

--- modules/initNewComment.js
+++ modules/initNewComment.js
@@ -2,7 +2,7 @@ import { sanitizeHtml } from './sanitizeHtml.js'
 import { renderComments } from './renderComments.js'
 import { text } from './renderComments.js'
 import { name } from './renderComments.js'
-import { postComment } from './api.js'
+import { fetchComments, postComment } from './api.js'
 import { updateComments } from './commentsArr.js'
 
 export const initNewComment = () => {
@@ -15,18 +15,20 @@ export const initNewComment = () => {
             document.querySelector('.form-loading').style.display = 'block'
             document.querySelector('.add-form').style.display = 'none'
 
-            postComment(
-                sanitizeHtml(text.value),
-                sanitizeHtml(name.value),
-            ).then((data) => {
-                document.querySelector('.form-loading').style.display = 'none'
-                document.querySelector('.add-form').style.display = 'flex'
+            postComment(sanitizeHtml(text.value), sanitizeHtml(name.value))
+                .then(() => {
+                    return fetchComments()
+                })
+                .then((data) => {
+                    document.querySelector('.form-loading').style.display =
+                        'none'
+                    document.querySelector('.add-form').style.display = 'flex'
 
-                updateComments(data)
-                renderComments()
-                name.value = ''
-                text.value = ''
-            })
+                    updateComments(data)
+                    renderComments()
+                    name.value = ''
+                    text.value = ''
+                })
         }
     })
 }
