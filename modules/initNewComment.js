8c2862dd7091e1e340e3e5f3bc39e8a55810619e
jerrye987-bit <jerrye987@gmail.com>
2026-06-07 20:37:25 +0500

    Сделана обработка ошибки 500. 3 попытки загрузки

--- modules/initNewComment.js
+++ modules/initNewComment.js
@@ -4,67 +4,86 @@ import { text } from './renderComments.js'
 import { name } from './renderComments.js'
 import { fetchComments, postComment } from './api.js'
 import { updateComments } from './commentsArr.js'
+import { delay } from './delay.js'
 
 export const initNewComment = () => {
     const addButton = document.querySelector('.add-form-button')
 
     addButton.addEventListener('click', () => {
-        if (name.value.trim() === '' || text.value.trim() === '') {
+        const nameInputElement = document.querySelector('.add-form-name') 
+        const textInputElement = document.querySelector('.add-form-text')
+
+        if (nameInputElement.value.trim() === '' || textInputElement.value.trim() === '') {
             return alert('Пожалуйста, заполните все поля формы!')
         } else {
             document.querySelector('.form-loading').style.display = 'block'
             document.querySelector('.add-form').style.display = 'none'
 
-            postComment(sanitizeHtml(text.value), sanitizeHtml(name.value))
-                .then((response) => {
-                    if (response.status === 201) {
-                        return response.json()
-                    } else {
-                        if (response.status === 500) {
-                            throw new Error('Ошибка сервера')
+            let attemptCount = 0
+
+            const handlePostClick = () => {
+                attemptCount++
+
+                postComment(sanitizeHtml(textInputElement.value), sanitizeHtml(nameInputElement.value))
+                    .then((response) => {
+                        if (response.status === 201) {
+                            return response.json()
+                        } else {
+                            if (response.status === 500) {
+                                throw new Error('Ошибка сервера')
+                            }
+
+                            if (response.status === 400) {
+                                throw new Error('Неверный запрос')
+                            }
+                        }
+                    })
+                    .then(() => {
+                        return fetchComments()
+                    })
+                    .then((data) => {
+                        document.querySelector('.form-loading').style.display = 'none'
+                        document.querySelector('.add-form').style.display = 'flex'
+
+                        updateComments(data)
+                        renderComments()
+
+                        nameInputElement.value = ''
+                        textInputElement.value = ''
+                    })
+                    .catch((error) => {
+                        document.querySelector('.form-loading').style.display = 'none'
+                        document.querySelector('.add-form').style.display = 'flex'
+
+                        if (error.message === 'Ошибка сервера' && attemptCount < 3) {
+                            console.warn(`Сервер упал. Попытка ${attemptCount} из 3. Пробуем еще раз через 2 секунды...`)
+                        
+                            delay().then(() => {
+                                handlePostClick()
+                            })
+                        } else {
+                            document.querySelector('.form-loading').style.display = 'none'
+                            document.querySelector('.add-form').style.display = 'flex'
+
+                            if (error.message === 'Неверный запрос') {
+                            alert('Имя и комментарий должны быть не короче трёх символов');
+
+                            nameInputElement.classList.add("-error");
+                            textInputElement.classList.add("-error");
+
+                            setTimeout(() => {
+                                nameInputElement.classList.remove("-error");
+                                textInputElement.classList.remove("-error");
+                            }, 500)
                         }
 
-                        if (response.status === 400) {
-                            throw new Error('Неверный запрос')
+                            if (error.message === 'Failed to fetch') {
+                                alert('Нет интернета, попробуйте позже');
+                            }
                         }
-                    }
-                })
-                .then(() => {
-                    return fetchComments()
-                })
-                .then((data) => {
-                    document.querySelector('.form-loading').style.display = 'none'
-                    document.querySelector('.add-form').style.display = 'flex'
-
-                    updateComments(data)
-                    renderComments()
-                    name.value = ''
-                    text.value = ''
-                })
-                .catch((error) => {
-                    document.querySelector('.form-loading').style.display = 'none'
-                    document.querySelector('.add-form').style.display = 'flex'
-
-                    if (error.message === 'Failed to fetch') {
-                        alert('Нет интернета, попробуйте позже');
-                    }
-
-                    if (error.message === 'Ошибка сервера') {
-                        alert(error.message);
-                    }
-
-                    if (error.message === 'Неверный запрос') {
-                        alert('Имя и комментарий должны быть не короче трёх символов');
-
-                        name.classList.add("-error");
-                        text.classList.add("-error");
-
-                        setTimeout(() => {
-                            name.classList.remove("-error");
-                            text.classList.remove("-error");
-                        }, 2000)
-                    }
-                })
+                    })
+                }
+            handlePostClick()
         }
     })
 }
