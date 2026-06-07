d665ba9aa48341d26d2be3b6af72df5450a27244
jerrye987-bit <jerrye987@gmail.com>
2026-06-07 18:53:26 +0500

    Сделана обработка ошибок

--- modules/initNewComment.js
+++ modules/initNewComment.js
@@ -16,18 +16,54 @@ export const initNewComment = () => {
             document.querySelector('.add-form').style.display = 'none'
 
             postComment(sanitizeHtml(text.value), sanitizeHtml(name.value))
+                .then((response) => {
+                    if (response.status === 201) {
+                        return response.json()
+                    } else {
+                        if (response.status === 500) {
+                            throw new Error('Ошибка сервера')
+                        }
+
+                        if (response.status === 400) {
+                            throw new Error('Неверный запрос')
+                        }
+                    }
+                })
                 .then(() => {
                     return fetchComments()
                 })
                 .then((data) => {
-                    document.querySelector('.form-loading').style.display =
-                        'none'
+                    document.querySelector('.form-loading').style.display = 'none'
                     document.querySelector('.add-form').style.display = 'flex'
 
                     updateComments(data)
                     renderComments()
                     name.value = ''
                     text.value = ''
+                })
+                .catch((error) => {
+                    document.querySelector('.form-loading').style.display = 'none'
+                    document.querySelector('.add-form').style.display = 'flex'
+
+                    if (error.message === 'Failed to fetch') {
+                        alert('Нет интернета, попробуйте позже');
+                    }
+
+                    if (error.message === 'Ошибка сервера') {
+                        alert(error.message);
+                    }
+
+                    if (error.message === 'Неверный запрос') {
+                        alert('Имя и комментарий должны быть не короче трёх символов');
+
+                        name.classList.add("-error");
+                        text.classList.add("-error");
+
+                        setTimeout(() => {
+                            name.classList.remove("-error");
+                            text.classList.remove("-error");
+                        }, 2000)
+                    }
                 })
         }
     })
